4b665b95a27f4a7ab6e11017f272a85d63a25a49
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2020-10-18 14:33:20 +0200

    add whole layout

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,8 @@ import '../scss/app.scss';
 import 'bootstrap';
 // import 'bootstrap/js/dist/util';
 // import 'bootstrap/js/dist/dropdown';
+
+$('#myTab a').on('click', (e) => {
+  e.preventDefault();
+  $(this).tab('show');
+});
