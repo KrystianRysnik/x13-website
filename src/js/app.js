8677c9a92750cb2752f94e11eb2a3de2a785143a
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2020-10-18 14:33:33 +0200

    add counter

--- src/js/app.js
+++ src/js/app.js
@@ -9,3 +9,34 @@ $('#myTab a').on('click', (e) => {
   e.preventDefault();
   $(this).tab('show');
 });
+
+function start_count() {
+  $('.numbers__counter').each(function() {
+    $(this)
+      .prop('Counter', 0)
+      .animate(
+        {
+          Counter: $(this).text(),
+        },
+        {
+          duration: 3000,
+          easing: 'swing',
+          step: function(now) {
+            $(this).text(Math.ceil(now));
+          },
+        }
+      );
+  });
+}
+
+$(function() {
+  let done = false;
+  let counterPosition = $('.numbers').offset().top - (window.innerHeight / 2);
+  $(window).on('scroll', function () {
+    let userPosition = $(window).scrollTop();
+    if (userPosition > counterPosition && !done) {
+      done = true;
+      start_count();
+    }
+  });
+});
