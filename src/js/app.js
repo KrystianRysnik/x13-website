036fde6849b93e08faaec6fa00f34b562dc8a1fd
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2020-10-19 18:31:59 +0200

    add callback and debounce

--- src/js/app.js
+++ src/js/app.js
@@ -13,9 +13,24 @@ $('#dropdownLogin').on('hide.bs.dropdown', function () {
   $('.overlay').removeClass('show');
 });
 
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 
 function start_count() {
-  $('.numbers__counter').each(function() {
+  $('.numbers__counter').each(function () {
     $(this)
       .prop('Counter', 0)
       .animate(
@@ -25,22 +40,30 @@ function start_count() {
         {
           duration: 3000,
           easing: 'swing',
-          step: function(now) {
+          step: function (now) {
             $(this).text(Math.ceil(now));
           },
         }
       );
-  });
+  }).promise().done( function () {
+    $('.numbers__counter--plus').each(function () {
+      $(this).text($(this).text() + '+');
+    })});
+
+
 }
 
-$(function() {
+$(function () {
   let done = false;
-  let counterPosition = $('.numbers').offset().top - (window.innerHeight / 2);
-  $(window).on('scroll', function () {
-    let userPosition = $(window).scrollTop();
-    if (userPosition > counterPosition && !done) {
-      done = true;
-      start_count();
-    }
-  });
+  let counterPosition = $('.numbers').offset().top - window.innerHeight / 2;
+  $(window).on(
+    'scroll',
+    debounce(function () {
+      let userPosition = $(window).scrollTop();
+      if (userPosition > counterPosition && !done) {
+        done = true;
+        start_count();
+      }
+    }, 100)
+  );
 });
