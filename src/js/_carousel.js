4dd20144bad5fb50048532fec5877cc069b704cd
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2020-10-20 00:33:03 +0200

    add slick carousel instead of css animate

---
+++ src/js/_carousel.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $('.partners__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 530,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ import '../scss/app.scss';
 
 // Your JS Code goes here
 import 'bootstrap';
+import 'slick-carousel';
 
 import './_counter';
 import './_dropdown';
+import './_carousel';
